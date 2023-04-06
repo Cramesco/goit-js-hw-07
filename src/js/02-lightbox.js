f7967692e10f480195e31d03a764d174ed6e9b14
Cramesco <110572929+Cramesco@users.noreply.github.com>
2023-04-06 13:36:27 -0500

    Modifying the ligthbox.js file, adding function and creating a new SimpleLigthbox object

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,32 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 
-const containerGallery = document.querySelector(".gallery");
+const galleryContainer = document.querySelector('ul.gallery');
+const photosMarkup = createGalleryItem(galleryItems);
 
-const galleryMarkup = galleryItems
-  .map(
-    (element) =>
-      `<li class= "gallery__item">
-      <a class= "gallery__link" href="${element.original}">
+function createGalleryItem(element) {
+    return element
+        .map(({ preview, original, description }) => {
+            return `
+        <li class='gallery_item'>
+        <a class= "gallery__link" href="${original}">
         <img
-            class= "gallery__image"
-            src= "${element.preview}"
-            alt= "${element.description}"
-          />
-      </a>
-    </li>`
-  )
-  .join("");
+        class = "gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+        })
 
-containerGallery.insertAdjacentHTML("afterbegin", galleryMarkup);
-containerGallery.addEventListener('click', (event) => event.preventDefault())
+        .join('');
+};
 
-//uso de la libreria SimplelightBox
 
-let lightbox = new SimpleLightbox('.gallery a', { 
-    captionsData: 'alt',
+
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+
+//Aqui usamos simplelightbox
+
+var lightbox = new SimpleLightbox(".gallery a", {
     captionDelay: 250,
- });
-
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+});
+lightbox.on('show.simplelightbox')
